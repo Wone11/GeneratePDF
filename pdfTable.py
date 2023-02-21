62ac71f8bcd4a47f2dca206640766b84f5afd21d
Wone11 <gelayegeremew@gmail.com>
2023-02-21 15:21:57 +0300

    Pie chart drawing tasks done

--- pdfTable.py
+++ pdfTable.py
@@ -2,6 +2,12 @@ from reportlab.platypus import Table ,Paragraph , SimpleDocTemplate
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
+'''graphics liberaries imported'''
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+
+
+
 '''fruits list'''
 fruits = {
         "Elderberries":1,
@@ -14,6 +20,25 @@ fruits = {
     }
 
 try:   
+    '''graphics intiations'''
+    inch=12
+    report_pie=Pie(width = 3*inch ,height=3*inch)
+    report_pie.sideLabels=True
+
+    report_pie_data=[]
+    report_pie_labels=[]
+    for fruit_name in sorted(fruits):
+        report_pie_data.append(fruits[fruit_name])
+        report_pie_labels.append(fruit_name)
+
+
+    #draw pie chart 
+    report_pie.data = report_pie_data
+    report_pie.labels=report_pie_labels
+    report_chart = Drawing()
+    report_chart.add(report_pie)
+
+    '''table data and styles'''
     table_data=[]
     report = SimpleDocTemplate('Fruits.pdf')
     style= getSampleStyleSheet()
@@ -23,7 +48,7 @@ try:
         table_data.append([k,v])
         
     report_table = Table(data=table_data, style=table_stles,hAlign="LEFT")
-    report.build([report_title,report_table])
+    report.build([report_title,report_table,report_chart])
 
 except Exception as error:
     print("Error happened during the pdf table creation!",error)
